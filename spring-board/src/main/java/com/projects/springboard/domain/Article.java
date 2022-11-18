8f5539c6ebdce3ecddf48ccdfd028b186764af2e
ysbc1247 <ystc1247@khu.ac.kr>
2022-11-18 14:24:46 +0900

    #8 - class domain architecture
    classes

--- spring-board/src/main/java/com/projects/springboard/domain/Article.java
+++ spring-board/src/main/java/com/projects/springboard/domain/Article.java
@@ -5,7 +5,7 @@ import java.time.LocalDateTime;
 public class Article {
     private Long id;
     private String title;
-    private String content;
+    private String content; //
     private String hashtag;
     private LocalDateTime createdAt;
     private String createdBy;
--- spring-board/src/main/java/com/projects/springboard/domain/ArticleComment.java
+++ spring-board/src/main/java/com/projects/springboard/domain/ArticleComment.java
@@ -5,7 +5,7 @@ import java.time.LocalDateTime;
 public class ArticleComment {
     private Long id;
     private Article article;
-    private String content;
+    private String content; //
     
     private LocalDateTime createdAt;
     private String createdBy;
