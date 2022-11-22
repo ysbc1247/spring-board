78395f9b5ead10adf63764adb445e484b872ac57
ysbc1247 <ystc1247@khu.ac.kr>
2022-11-22 16:00:23 +0900

    Mistook branch, use branch master until next feature

--- spring-board/src/main/java/com/projects/springboard/domain/Article.java
+++ spring-board/src/main/java/com/projects/springboard/domain/Article.java
@@ -1,7 +1,13 @@
 package com.projects.springboard.domain;
 
-import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.ToString;
 
+import javax.persistence.Table;
+import java.time.LocalDateTime;
+@Getter
+@ToString
+@Table
 public class Article {
     private Long id;
     private String title;
