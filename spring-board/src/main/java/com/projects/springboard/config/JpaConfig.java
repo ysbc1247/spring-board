4cb28dad62454b73b61a33b39d69dc16853409c6
ysbc1247 <ystc1247@khu.ac.kr>
2022-11-22 16:21:15 +0900

    Working on JPA configurations, structures

---
+++ spring-board/src/main/java/com/projects/springboard/config/JpaConfig.java
@@ -0,0 +1,17 @@
+package com.projects.springboard.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.Optional;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+    @Bean
+    public AuditorAware<String> auditorAware(){
+        return () -> Optional.of("ystc1247"); //TODO: temporary data
+    }
+}
--- spring-board/src/main/java/com/projects/springboard/domain/Article.java
+++ spring-board/src/main/java/com/projects/springboard/domain/Article.java
@@ -1,21 +1,45 @@
 package com.projects.springboard.domain;
 
 import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
 
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 @Getter
 @ToString
-@Table
+@Table(indexes = {
+        @Index(columnList = "title"),
+        @Index(columnList = "hashtag"),
+        @Index(columnList = "createdAt"),
+        @Index(columnList = "createdBy")
+})
 public class Article {
-    private Long id;
-    private String title;
-    private String content; //
-    private String hashtag;
-    private LocalDateTime createdAt;
-    private String createdBy;
-    private LocalDateTime modifiedAt;
-    private String modifiedBy;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
 
+    private Long id;
+    @Setter @Column(nullable = false) private String title;
+    @Setter @Column(nullable = false, length = 10000) private String content; //
+
+    @Setter private String hashtag;
+
+    @CreatedDate @Column(nullable = false) private LocalDateTime createdAt;
+    @CreatedBy @Column(nullable = false, length = 100) private String createdBy;
+    @LastModifiedDate @Column(nullable = false) private LocalDateTime modifiedAt;
+    @LastModifiedBy @Column(nullable = false, length = 100  ) private String modifiedBy;
+
+    private Article(String title, String content, String hashtag){
+        this.title = title;
+        this.content = content;
+        this.hashtag = hashtag;
+    }
+
+    public static Article of(String title, String content, String hashtag) {
+        return new Article(title, content, hashtag);
+    }
 }
