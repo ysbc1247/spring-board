37f42b1e41ad8121623c3878461edb6febb406fa
ysbc1247 <ystc1247@khu.ac.kr>
2022-12-05 02:24:17 +0900

    Remove overlapping lines from classes

--- spring-board/src/main/java/com/projects/springboard/domain/ArticleComment.java
+++ spring-board/src/main/java/com/projects/springboard/domain/ArticleComment.java
@@ -24,7 +24,7 @@ import java.util.Set;
 })
 @Entity
 
-public class ArticleComment {
+public class ArticleComment extends AuditingFields{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -39,14 +39,6 @@ public class ArticleComment {
 
     private Set<ArticleComment> articleComments = new LinkedHashSet<>();
 
-    @CreatedDate
-    @Column(nullable = false) private LocalDateTime createdAt;
-    @CreatedBy
-    @Column(nullable = false, length = 100) private String createdBy;
-    @LastModifiedDate
-    @Column(nullable = false) private LocalDateTime modifiedAt;
-    @LastModifiedBy
-    @Column(nullable = false, length = 100  ) private String modifiedBy;
 
     protected ArticleComment() {}
     private ArticleComment(Article article, String content) {
