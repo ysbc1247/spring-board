94b8d1445682b14141fc337160cfe6cff97fa3b4
ysbc1247 <ystc1247@khu.ac.kr>
2022-12-05 02:44:10 +0900

    Mark conflicts

--- spring-board/src/main/java/com/projects/springboard/repository/ArticleCommentRepository.java
+++ spring-board/src/main/java/com/projects/springboard/repository/ArticleCommentRepository.java
@@ -1,6 +1,9 @@
 package com.projects.springboard.repository;
 import com.projects.springboard.domain.ArticleComment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
 public interface ArticleCommentRepository extends JpaRepository<ArticleComment,Long>{
 
 }
