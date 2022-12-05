79a8951a090d6c84bf080f8d8f641a819c2fd7f0
ysbc1247 <ystc1247@khu.ac.kr>
2022-12-05 15:50:38 +0900

    Ssc

---
+++ spring-board/src/test/java/com/projects/springboard/controller/DataRestTest.java
@@ -0,0 +1,70 @@
+package com.projects.springboard.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+@DisplayName("DataRestTest")
+@Transactional
+@AutoConfigureMockMvc
+@SpringBootTest
+public class DataRestTest {
+    private final MockMvc mvc;
+
+    public DataRestTest(@Autowired MockMvc mvc) {
+        this.mvc = mvc;
+    }
+    @DisplayName("[api] Article List LookUp")
+    @Test
+    void givenNothing_whenRequestingArticles_thenReturnsJsonResponse() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/api/articles"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.valueOf("application/hal+json")));
+
+
+    }
+    @DisplayName("[api] Article List LookUp By One")
+    @Test
+    void givenNothing_whenRequestingArticlesByOne_thenReturnsJsonResponse() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/api/articles/1"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.valueOf("application/hal+json")));
+
+
+    }
+    @DisplayName("[api] ArticleComments by Article List LookUp")
+    @Test
+    void givenNothing_whenRequestingArticleCommentsFromArticle_thenReturnsJsonResponse() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/api/articles/1/articleComments"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.valueOf("application/hal+json")));
+
+
+    }
+    @DisplayName("[api] ArticleComments List LookUp")
+    @Test
+    void givenNothing_whenRequestingArticlesComments_thenReturnsJsonResponse() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/api/articleComments"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.valueOf("application/hal+json")));
+
+
+    }
+    @DisplayName("[api] ArticleComment LookUp")
+    @Test
+    void givenNothing_whenRequestingArticlesComment_thenReturnsJsonResponse() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/api/articleComments/1"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.valueOf("application/hal+json")));
+    }
+}
