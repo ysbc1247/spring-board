6451c19763e18e7e08fa3cd10970610851b37d50
ysbc1247 <ystc1247@khu.ac.kr>
2022-12-07 11:27:27 +0900

    #1- modify Article,Comments to be able to search contents

--- spring-board/src/main/java/com/projects/springboard/repository/ArticleRepository.java
+++ spring-board/src/main/java/com/projects/springboard/repository/ArticleRepository.java
@@ -1,9 +1,30 @@
 package com.projects.springboard.repository;
 
 import com.projects.springboard.domain.Article;
+import com.projects.springboard.domain.QArticle;
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
+import org.hibernate.criterion.SimpleExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import javax.print.DocFlavor;
+
 @RepositoryRestResource
-public interface ArticleRepository extends JpaRepository<Article, Long> {
+public interface ArticleRepository extends
+        JpaRepository<Article, Long>,
+        QuerydslPredicateExecutor<Article>,
+        QuerydslBinderCustomizer<QArticle>{
+     @Override
+    default void customize(QuerydslBindings bindings, QArticle root){
+        bindings.excludeUnlistedProperties(true);
+        bindings.including(root.title, root.hashtag, root.createdAt, root.createdBy, root.content);
+        bindings.bind(root.title).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.hashtag).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.createdAt).first(DateTimeExpression::eq);
+        bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
+    }
 }
