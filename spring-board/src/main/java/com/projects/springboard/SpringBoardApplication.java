cf21c8065de8e2b9d5cbd45501a03739948bdadd
ysbc1247 <ystc1247@khu.ac.kr>
2022-12-13 06:04:04 +0900

    #16 - add map for details

--- spring-board/src/main/java/com/projects/springboard/SpringBoardApplication.java
+++ spring-board/src/main/java/com/projects/springboard/SpringBoardApplication.java
@@ -2,8 +2,10 @@ package com.projects.springboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class SpringBoardApplication {
 
 	public static void main(String[] args) {
