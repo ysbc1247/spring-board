73e159ded7442e485f492844f80c53a7e3b6d8a9
ysbc1247 <ystc1247@khu.ac.kr>
2022-12-13 14:01:50 +0900

    #16 - Imported SecurityConfig.class

--- spring-board/src/test/java/com/projects/springboard/controller/ArticleControllerTest.java
+++ spring-board/src/test/java/com/projects/springboard/controller/ArticleControllerTest.java
@@ -1,10 +1,12 @@
 package com.projects.springboard.controller;
 
+import com.projects.springboard.config.SecurityConfig;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -15,23 +17,24 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @DisplayName("View Controller - Articles")
+@Import(SecurityConfig.class)
+
 @WebMvcTest(ArticleController.class)
 class ArticleControllerTest {
         private MockMvc mvc;
 
         public ArticleControllerTest(@Autowired MockMvc mvc){
-            this.mvc = mvc;
+                this.mvc = mvc;
         }
 
-        
         @Test
         @DisplayName("[view][GET] Article List - 정상 호출")
         public void givenNothing_whenRequestingArticlesView_thenReturnsArticlesView() throws Exception {
-            mvc.perform(get("/articles"))
-                    .andExpect(status().isOk())
-                    .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
-                    .andExpect(view().name("articles/index"))
-                    .andExpect(model().attributeExists("articles"));
+                mvc.perform(get("/articles"))
+                        .andExpect(status().isOk())
+                        .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+                        .andExpect(view().name("articles/index"))
+                        .andExpect(model().attributeExists("articles"));
 
         }
 
@@ -48,23 +51,25 @@ class ArticleControllerTest {
                         .andExpect(model().attributeExists("article"))
                         .andExpect(model().attributeExists("articleComments"));
         }
+
         @Disabled("implementing")
         @Test
         @DisplayName("[view][GET] Article Looking up Page - 정상 호출")
         public void givenNothing_whenRequestingArticlesSearchView_thenReturnsArticlesSearchView() throws Exception {
-            mvc.perform(get("/articles/search"))
-                    .andExpect(status().isOk())
-                    .andExpect(view().name("articles/search"))
-                    .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML));
+                mvc.perform(get("/articles/search"))
+                        .andExpect(status().isOk())
+                        .andExpect(view().name("articles/search"))
+                        .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML));
         }
+
         @Disabled("implementing")
         @Test
         @DisplayName("[view][GET] Article Hashtag Search - 정상 호출")
         public void givenNothing_whenRequestingHashtagSearchView_thenReturnsHashtagSearchView() throws Exception {
-            mvc.perform(get("/articles/search-hashtag"))
-                    .andExpect(status().isOk())
-                    .andExpect(view().name("articles/hashtag"))
-                    .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML));
+                mvc.perform(get("/articles/search-hashtag"))
+                        .andExpect(status().isOk())
+                        .andExpect(view().name("articles/hashtag"))
+                        .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML));
         }
 
 }
--- spring-board/src/test/java/com/projects/springboard/controller/AuthControllerTest.java
+++ spring-board/src/test/java/com/projects/springboard/controller/AuthControllerTest.java
@@ -1,10 +1,12 @@
 package com.projects.springboard.controller;
 
+import com.projects.springboard.config.SecurityConfig;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -13,6 +15,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 
 @DisplayName("View Controller - authorization")
+@Import(SecurityConfig.class)
 @WebMvcTest
 public class AuthControllerTest {
     private MockMvc mvc;
