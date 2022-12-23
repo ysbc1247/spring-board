904f2e06768be2e0b630086091581c02c9bf5399
ysbc1247 <ystc1247@khu.ac.kr>
2022-12-23 18:30:05 +0900

    #21 - ArticleCommentServiceTest
    update, save, create

---
+++ spring-board/src/test/java/com/projects/springboard/service/ArticleCommentServiceTest.java
@@ -0,0 +1,65 @@
+package com.projects.springboard.service;
+
+import com.projects.springboard.domain.Article;
+import com.projects.springboard.domain.ArticleComment;
+import com.projects.springboard.dto.ArticleCommentDto;
+import com.projects.springboard.repository.ArticleCommentRepository;
+import com.projects.springboard.repository.ArticleRepository;
+import org.assertj.core.api.BDDAssertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import javax.persistence.EntityNotFoundException;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.*;
+import static org.mockito.BDDMockito.then;
+
+@DisplayName("Article Comments")
+@ExtendWith(MockitoExtension.class)
+
+class ArticleCommentServiceTest {
+    @InjectMocks private ArticleCommentService sut;
+
+    @Mock private ArticleRepository articleRepository;
+    @Mock private ArticleCommentRepository articleCommentRepository;
+
+    @DisplayName("게시글 ID로 조회하면, 해당하는 댓글 리스트를 반환한다.")
+    @Test
+    void givenArticleId_whenSearchingArticleComments_thenReturnsArticleComments() {
+        // Given
+        Long articleId = 1L;
+        given(articleRepository.findById(articleId)).willReturn(Optional.of(
+                Article.of("title", "content", "#java"))
+        );
+
+        // When
+        List<ArticleCommentDto> articleComments = sut.searchArticleComment(articleId);
+
+        // Then
+        assertThat(articleComments).isNotNull();
+        then(articleRepository).should().findById(articleId);
+    }
+
+    @DisplayName("댓글 정보를 입력하면, 댓글을 저장한다.")
+    @Test
+    void givenArticleCommentInfo_whenSavingArticleComment_thenSavesArticleComment() {
+        // Given
+        given(articleCommentRepository.save(any(ArticleComment.class))).willReturn(null);
+
+        // When
+        sut.saveArticleComment(ArticleCommentDto.of(LocalDateTime.now(), "Uno", LocalDateTime.now(), "Uno", "comment"));
+
+        // Then
+        then(articleCommentRepository).should().save(any(ArticleComment.class));
+    }
+}
