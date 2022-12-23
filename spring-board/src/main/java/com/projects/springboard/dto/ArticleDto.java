543582f7aa0d8fc07bd5522945aafec947fdca3f
ysbc1247 <ystc1247@khu.ac.kr>
2022-12-23 18:28:11 +0900

    #21 - Created Article Dto

---
+++ spring-board/src/main/java/com/projects/springboard/dto/ArticleDto.java
@@ -0,0 +1,17 @@
+package com.projects.springboard.dto;
+
+import com.projects.springboard.domain.Article;
+
+import java.time.LocalDateTime;
+
+public record ArticleDto(
+        LocalDateTime createdAt,
+        String createdBy,
+        String title,
+        String content,
+        String hashtag
+) {
+    public static ArticleDto of(LocalDateTime createdAt, String createdBy, String title, String content, String hashtag) {
+        return new ArticleDto(createdAt, createdBy, title, content, hashtag);
+    }
+}
