3a85a2ca220c4501412d0c5a2eb5253516d0b7d9
ysbc1247 <ystc1247@khu.ac.kr>
2022-12-23 18:30:52 +0900

    #21 - Main Controller Test for mvc

---
+++ spring-board/src/test/java/com/projects/springboard/controller/MainControllerTest.java
@@ -0,0 +1,28 @@
+package com.projects.springboard.controller;
+
+import com.projects.springboard.config.SecurityConfig;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@Import(SecurityConfig.class)
+@WebMvcTest(MainController.class)
+class MainControllerTest {
+    private final MockMvc mvc;
+
+    MainControllerTest(@Autowired MockMvc mvc) {
+        this.mvc = mvc;
+    }
+
+    @Test
+    void givenNothing_whenRequestingRootPage_thenRedirectsToArticlesPage() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/"))
+                .andExpect(MockMvcResultMatchers.status().is3xxRedirection());
+
+    }
+
+}
